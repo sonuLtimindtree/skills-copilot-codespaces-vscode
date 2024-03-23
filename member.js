57382601e9bd190a9f636df97f6d97952189671b
sonuLtimindtree <164135871+sonuLtimindtree@users.noreply.github.com>
2024-03-23 09:34:13 +0000

    copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember(){
+    return{
+        restrict:'E',
+        templateUrl:"",
+        controller:"",
+        controllas:"",
+        bindController:true,
+        acope:{
+            member:""
+        }
+
+    }
+}
